5e2e3a51eee0fa80a06af221ae32b6cfbef5bafd
juanlr15963 <juanlopezra15963@gmail.com>
2026-03-13 23:34:02 +0000

    fix: corección del tipo de anotación para mypy

--- core/expense_service.py
+++ core/expense_service.py
@@ -79,7 +79,7 @@ class ExpenseService:
         return suma
 
     def total_by_month(self) -> dict[str, float]:
-        totals = defaultdict[str, float] = defaultdict(float)
+        totals: dict[str, float] = defaultdict(float)
 
         for expense in self._repository.list_all():
             key = expense.expense_date.strftime("%Y-%m")
